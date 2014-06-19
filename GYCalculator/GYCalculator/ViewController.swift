fc16967a033958c5b94f603fca0a300c28a3ae26
Derek Li <derekli@zoaks.com>
2014-06-19 17:59:27 +0800

    [1] add artworks and setup IBOutlet

--- GYCalculator/GYCalculator/ViewController.swift
+++ GYCalculator/GYCalculator/ViewController.swift
@@ -9,7 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    @IBOutlet var button01 : UIButton
+    @IBOutlet var button02 : UIButton
+    @IBOutlet var button03 : UIButton
+    @IBOutlet var button04 : UIButton
+    @IBOutlet var button05 : UIButton
+    @IBOutlet var button06 : UIButton
+    @IBOutlet var button07 : UIButton
+    @IBOutlet var button08 : UIButton
+    @IBOutlet var button09 : UIButton
+
+    @IBOutlet var buttonZero : UIButton
+    @IBOutlet var buttonDot : UIButton
+    @IBOutlet var buttonPlus : UIButton
+    @IBOutlet var buttonMinus : UIButton
+    @IBOutlet var buttonMultiply  : UIButton
+    @IBOutlet var buttonDivide  : UIButton
+    @IBOutlet var buttonClean : UIButton
+    @IBOutlet var buttonOperation : UIButton
+    
+    @IBAction func addOperand(aBtn : UIButton){
+        NSLog("%d", aBtn.tag)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
